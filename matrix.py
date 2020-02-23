de6d195ea35820c1b60e219150aa06e53e8b8d32
angel ortmann-li <hyejinkyo@gmail.com>
2020-02-23 00:51:19 -0500

    made it multiply!

--- matrix.py
+++ matrix.py
@@ -7,14 +7,15 @@ y0  y1      yn
 z0  z1  ... zn
 1  1        1
 """
+
 import math
 
 def new_matrix(rows = 4, cols = 4):
     m = []
-    for c in range( cols ):
+    for r in range( rows ):
         m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
+        for c in range( cols ):
+            m[r].append( 0 )
     return m
 
 #print the matrix such that it looks like
@@ -30,20 +31,52 @@ def print_matrix( matrix ):
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
-    pass
+    iden = matrix
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if (r == c):
+                iden[r][c] = 1
+            else:
+                iden[r][c] = 0
+    return iden
+
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    prod = []
-    temp = 0
-    for h in range(len(m1)):
-        for i in range(4):
-            for w in range(len(m1[0])):
-            temp += m1[h][w]*m2[i][w]
-            prod.append(temp)
-            temp = 0
+    prod = new_matrix(len(m1),len(m2[0]))
+    if len(m1[0]) == len(m2):
+        for a in range(len(m1)):
+            for b in range(len(m2[0])):
+                for c in range(len(m2)):
+                    prod[a][b] += m1[a][c]*m2[c][b] 
+    else:
+        print("u are a buffoon")
+    print_matrix(prod)
     return prod
 
-g = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+
+g = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 print_matrix(g)
+print()
+print()
+
+f=ident(g)
+print_matrix(f)
+print()
+print()
+
+
+
+g = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+matrix_mult(g,f)
+print()
+print()
+j= [[2,0,0,0],[0,2,0,0],[0,0,2,0],[0,0,0,2]]
+matrix_mult(g,j)
+
+
+a= [[1.00,4.00],[2.00,5.00],[3.00,6.00],[1.00,1.00]]
+b= [[1.00,4.00,7.00,10.00],[2.00,5.00,8.00,11.00],[3.00,6.00,9.00,12.00],[1.00,1.00,1.00,1.00]]
+
+matrix_mult(b,a)
